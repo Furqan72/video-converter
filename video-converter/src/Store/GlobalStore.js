abf2fe76716bed6b899ba95d67383f43a536582b
Muhammad Furqan Haider <ranafurqanhaider5@gmail.com>
2023-11-13 15:09:43 +0500

    test

--- video-converter/src/Store/GlobalStore.js
+++ video-converter/src/Store/GlobalStore.js
@@ -63,8 +63,8 @@ export const useGlobalStore = defineStore('GlobalStore', () => {
     // console.log(formData);
 
     axios
-      // .post('https://video-converter-api.vercel.app/test', formData, {
-      .post('http://localhost:8080/test', formData, {
+      .post('https://video-converter-api.vercel.app/test', formData, {
+        // .post('http://localhost:8080/test', formData, {
         headers: {
           'Content-Type': 'multipart/form-data',
         },
@@ -76,9 +76,9 @@ export const useGlobalStore = defineStore('GlobalStore', () => {
       .then((response) => {
         console.log(response.data);
 
+        downloadUrlFromNode.value = 'https://video-converter-api.vercelx.app/' + response.data.downloadUrl;
+        // downloadUrlFromNode.value = 'http://localhost:8080/' + response.data.downloadUrl;
         // downloadUrlFromNode.value = 'https://video-converter-api.vercel.app/temp-output/kb-image.jpg';
-        downloadUrlFromNode.value = 'http://localhost:8080/' + response.data.downloadUrl;
-        // downloadUrlFromNode.value = 'https://video-converter-api.vercel.app/' + response.data.downloadUrl;
 
         downloadName.value = response.data.fileName;
         // downloadName.value = 'kb-image.jpg';
